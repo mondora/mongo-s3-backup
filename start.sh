cd9c26203630281134db19f0e16899edeb6586f8
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-06-11 16:11:11 +0200

    Pass environmnet to script run by cron

--- start.sh
+++ start.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-: ${AWS_ACCESS_KEY_ID:?"env variable is required"}
-: ${AWS_SECRET_ACCESS_KEY:?"env variable is required"}
-: ${MONGO_HOST:?"env variable is required"}
-: ${MONGO_PORT:?"env variable is required"}
-: ${S3_BUCKET:?"env variable is required"}
-: ${S3_FOLDER:?"env variable is required"}
-export AWS_DEFAULT_REGION=$(python /script/get_bucket_region.py $S3_BUCKET)
-export BACKUP_FILENAME_DATE_FORMAT=${BACKUP_FILENAME_DATE_FORMAT:-%Y%m%d}
-export BACKUP_FILENAME_PREFIX=${BACKUP_FILENAME_PREFIX:-mongo_backup}
-export CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 
+CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
+CRON_ENVIRONMENT="
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?"env variable is required"}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?"env variable is required"}
+MONGO_HOST=${MONGO_HOST:?"env variable is required"}
+MONGO_PORT=${MONGO_PORT:?"env variable is required"}
+S3_BUCKET=${S3_BUCKET:?"env variable is required"}
+S3_FOLDER=${S3_FOLDER:?"env variable is required"}
+AWS_DEFAULT_REGION=$(python /script/get_bucket_region.py $S3_BUCKET)
+BACKUP_FILENAME_DATE_FORMAT=${BACKUP_FILENAME_DATE_FORMAT:-%Y%m%d}
+BACKUP_FILENAME_PREFIX=${BACKUP_FILENAME_PREFIX:-mongo_backup}
+"
 echo
 echo "Configuration"
 echo
-echo "MONGO_HOST:                     $MONGO_HOST"
-echo "MONGO_PORT:                     $MONGO_PORT"
-echo "AWS_ACCESS_KEY_ID:              $AWS_ACCESS_KEY_ID"
-echo "AWS_SECRET_ACCESS_KEY:          $AWS_SECRET_ACCESS_KEY"
-echo "S3_BUCKET:                      $S3_BUCKET"
-echo "S3_FOLDER:                      $S3_FOLDER"
-echo "AWS_DEFAULT_REGION:             $AWS_DEFAULT_REGION"
-echo "BACKUP_FILENAME_DATE_FORMAT:    $BACKUP_FILENAME_DATE_FORMAT"
-echo "BACKUP_FILENAME_PREFIX:         $BACKUP_FILENAME_PREFIX"
-echo "CRON_SCHEDULE:                  $CRON_SCHEDULE"
+echo "CRON_SCHEDULE"
 echo
+echo "$CRON_SCHEDULE"
+echo
+echo "CRON_ENVIRONMENT"
+echo "$CRON_ENVIRONMENT"
 
-echo "$CRON_SCHEDULE /script/backup.sh" | crontab -
+echo "$CRON_ENVIRONMENT $CRON_SCHEDULE /script/backup.sh" | crontab -
 exec cron -f
